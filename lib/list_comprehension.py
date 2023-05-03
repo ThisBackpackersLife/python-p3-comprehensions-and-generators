7528eca7c60e5292774ecc789744106cd7886441
Nicholas <crazedglitch@yahoo.com>
2023-05-03 14:28:41 -0500

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_list = []
+    for number in num_list:
+        if number % 2 == 0:
+            evens_list.append(number)
+    return evens_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [ i + "!" for i in sentence_list]
+    return new_list
+
